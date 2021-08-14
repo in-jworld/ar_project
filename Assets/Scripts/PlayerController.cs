3ef506bb7b72de2034f5d1795169aabf9dcb239b
jworld <j.world.au@gmail.com>
2021-08-14 14:23:30 +1000

    re-implement player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    public float playerSpeed = 5f;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = player.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Move(Vector2 inputDirection)
+    {
+        Vector3 moveInput = inputDirection;//new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        bool isMoving = moveInput.magnitude != 0;
+        anim.SetBool("isMoving", isMoving);
+        if (isMoving)
+        {
+            Vector3 moveDir = new Vector3(moveInput.x, 0f, moveInput.y);
+            transform.position += moveDir * Time.deltaTime * playerSpeed;
+        }
+    }
+}
---
+++ Assets/Scripts/VirtualJoystick.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class VirtualJoystick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [SerializeField] private RectTransform lever;
+    private RectTransform rectTransform;
+
+    [SerializeField, Range(10, 150)] private float leverRange = 65;
+
+    private Vector2 inputDirection = Vector2.zero;
+    private bool isInput = false;
+
+    [SerializeField] private PlayerController controller;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        ControlJoystickLever(eventData);
+        isInput = true;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        ControlJoystickLever(eventData);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        lever.anchoredPosition = Vector2.zero;
+        isInput = false;
+        controller.Move(Vector2.zero);
+    }
+
+    private void ControlJoystickLever(PointerEventData eventData)
+    {
+        var inputPos = eventData.position - rectTransform.anchoredPosition;
+        var inputVector = inputPos.magnitude < leverRange ? inputPos : inputPos.normalized * leverRange;
+        lever.anchoredPosition = inputVector;
+        inputDirection = inputVector / leverRange;
+    }
+
+    private void InputControlVector()
+    {
+        controller.Move(inputDirection);
+        Debug.Log(inputDirection.x + " / " + inputDirection.y);
+    }
+
+    private void Update()
+    {
+        if(isInput)
+        {
+            InputControlVector();
+        }
+    }
+}
